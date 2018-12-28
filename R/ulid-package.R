4e46082caecb88ed9e483985cdd88d6570898fb5
boB Rudis <bob@rud.is>
2018-12-28 11:03:38 -0500

    R package repo initialization complete

---
+++ R/ulid-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/ulid>
+#' - BugReports: <https://gitlab.com/hrbrmstr/ulid/issues>
+#' 
+#' @md
+#' @name ulid
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
