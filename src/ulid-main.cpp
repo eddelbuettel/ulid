5607e0706ca0bd81267d2f4aac859cd989435a37
boB Rudis <bob@rud.is>
2018-12-28 14:00:35 -0500

    unmarshalling

--- src/ulid-main.cpp
+++ src/ulid-main.cpp
@@ -22,3 +22,41 @@ CharacterVector ULIDgenerate(long n=1) {
   }
   return(c);
 }
+
+
+//' Unmarshal a ULID into a data frame with timestamp and random bitstring columns
+//'
+//' @md
+//' @param ulids character ULIDs (e.g. created with [ULIDgenerate()])
+//' @export
+//' @examples
+//' unmarshal(ULIDgenerate())
+// [[Rcpp::export]]
+DataFrame unmarshal(std::vector<std::string> ulids) {
+
+  unsigned long sz = ulids.size();
+  std::vector<Rcpp::Datetime> dt(sz);
+  std::vector<std::string> cv(sz);
+
+  for (unsigned long i=0; i<sz; i++) {
+
+    ulid::ULID u = ulid::Unmarshal(ulids[i]);
+
+    dt[i] = Rcpp::Datetime(ulid::Time(u));
+    cv[i] = ulids[i].substr(10);
+
+  }
+
+  DataFrame out =
+    DataFrame::create(
+      _["ts"] = dt,
+      _["rnd"] = cv,
+      _["stringsAsFactors"] = false
+    );
+
+  out.attr("class") = CharacterVector::create("tbl_df", "tbl", "data.frame");
+
+  return(out);
+
+}
+
