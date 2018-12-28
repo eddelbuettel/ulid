3e8136fe29ea97ff15083e221e3b008ae4e76cca
boB Rudis <bob@rud.is>
2018-12-28 11:58:41 -0500

    initial commit

---
+++ src/ulid.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// http://stackoverflow.com/a/23981011
+#ifdef __SIZEOF_INT128__
+#define ULIDUINT128
+#endif
+
+#ifdef ULIDUINT128
+#include "ulid_uint128.h"
+#else
+#include "ulid_struct.h"
+#endif // ULIDUINT128
